7d41ef6c7a35cf8ecadc7db3f0ed0ad8ae0bda6f
Erick <erickloboabreu@hotmail.com>
2025-10-30 15:45:42 -0300

    add mustache partials and main

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,7 @@
 import type { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-  //res.render('pages/page')
-  res.send('home no controller!')
+  res.render('pages/page')
 }
 export const dogs = (req: Request, res: Response) => {
   //res.render('pages/page')
