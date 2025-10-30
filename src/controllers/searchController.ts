cfa7e0f4865ec7e0373b424428a9ca3f242a9c59
Erick <erickloboabreu@hotmail.com>
2025-10-30 17:51:49 -0300

    fix search when nothing is typed and delete html folder

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -6,6 +6,11 @@ export const search = (req: Request, res: Response) => {
   let query: string = req.query.q as string
   let list = Pet.getFromName(query)
 
+  if(!query){
+    res.redirect('/')
+    return;
+  }
+  
   res.render('pages/page', {
     menu: menuObject(''),
     list,
